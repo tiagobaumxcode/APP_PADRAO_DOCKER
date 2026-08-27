f184d79df73d558e8448b1394f8f7df61a55603e
tiagobaumxcode <tiagobaumxcode@gmail.com>
2026-08-27 16:37:45 -0300

    Vite e NPM funcionando perfeitamente.

--- APP_PADRAO/src/vite.config.js
+++ APP_PADRAO/src/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { bunny } from "laravel-vite-plugin/fonts";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
+                bunny("Instrument Sans", {
                     weights: [400, 500, 600],
                 }),
             ],
@@ -17,8 +17,13 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: "0.0.0.0",
+        port: 5173,
+        hmr: {
+            host: "localhost",
+        },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            usePolling: true,
         },
     },
 });
